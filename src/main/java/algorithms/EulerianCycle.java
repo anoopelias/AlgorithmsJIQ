7acf8c45a7cc88f87f0f0a5c4921626230dd043b
Anoop Elias <anoopelias@gmail.com>
2013-11-17 21:15:23 +0530

    Crude (But complete) implementation of Euler path

---
+++ src/main/java/algorithms/EulerianCycle.java
@@ -0,0 +1,150 @@
+package algorithms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.princeton.cs.algs4.Edge;
+import edu.princeton.cs.algs4.EdgeWeightedGraph;
+
+/**
+ * Eulierian path.
+ * 
+ * An Eulierian cycle in a graph is a cycle (not necessarily simple) that uses
+ * every edge in the graph exactly once.
+ * 
+ * Show that a graph has an Eulerian cycle if and only if every vertex has even
+ * degree and all of the vertices with nonzero degree belong to the same
+ * connected component.
+ * 
+ * Design a linear-time algorithm to determine whether a graph has an Eulerian
+ * cycle, and if so, find one.
+ * 
+ * @author Anoop Elias
+ * 
+ */
+public class EulerianCycle {
+
+	public boolean hasEulerianCycle;
+	public List<Integer> cycle;
+
+	public List<Edge> marked;
+
+	public EulerianCycle(EdgeWeightedGraph g) {
+		if (g.E() > 0) {
+			marked = new ArrayList<Edge>();
+			Edge e = g.edges().iterator().next();
+
+			int start = e.either();
+			cycle = walk(g, e, start, start);
+
+			while (cycle != null && marked.size() != g.E()) {
+				cycle = walkFromCycle(g, cycle);
+			}
+			
+			if(cycle != null)
+				cycle.add(start);
+
+			hasEulerianCycle = (cycle != null && marked.size() == g.E());
+		}
+	}
+
+	private List<Integer> walkFromCycle(EdgeWeightedGraph g, List<Integer> path) {
+		boolean updated = false;
+
+		for (Edge e : g.edges()) {
+
+			if (!marked.contains(e)) {
+				int v = e.either();
+				if (!path.contains(v))
+					v = e.other(v);
+
+				// Edge e has a point on the path.
+				if (path.contains(v)) {
+					List<Integer> newLoop = walk(g, e, v, v);
+					if (newLoop == null)
+						return null;
+
+					path.addAll(path.indexOf(v) + 1, newLoop);
+					updated = true;
+
+					break;
+				}
+			}
+		}
+
+		if (updated)
+			return path;
+		else
+			return null;
+	}
+
+	/**
+	 * Identify a path from 'from' to 'target' using edge 'e' as the first edge.
+	 * 
+	 * Returns null if the path can't be found.
+	 * 
+	 * @param g graph
+	 * @param e starting edge
+	 * @param from stating vertex
+	 * @param target target verted.
+	 * @return path from 'from' to 'target'
+	 */
+	private List<Integer> walk(EdgeWeightedGraph g, Edge e, int from, int target) {
+
+		marked.add(e);
+		int end = e.other(from);
+
+		// Completed the cycle
+		if (end == target)
+			return newPath(end);
+
+		for (Edge outEdge : g.adj(end)) {
+			if (!marked.contains(outEdge)) {
+
+				// Got a way out from this end
+				List<Integer> path = walk(g, outEdge, end, target);
+
+				// No way to complete the cycle
+				if (path == null)
+					return null;
+
+				path.add(0, end);
+				return path;
+			}
+		}
+
+		// No way out from 'end' vertex.
+		return null;
+
+	}
+
+	/** 
+	 * Create a new single-vertex path.
+	 * 
+	 * @param v
+	 * @return
+	 */
+	private List<Integer> newPath(int v) {
+		List<Integer> path = new ArrayList<Integer>();
+		path.add(v);
+		return path;
+	}
+
+	/**
+	 * @return whether the graph has an eulerian path or not.
+	 */
+	public boolean hasEulerianCycle() {
+		return hasEulerianCycle;
+	}
+
+	/**
+	 * @return the eulerian path if exists, else null
+	 */
+	public List<Integer> eulerianCycle() {
+		if (hasEulerianCycle)
+			return cycle;
+		else
+			return null;
+	}
+
+}
--- src/test/java/algorithms/EulerianPathTest.java
+++ src/test/java/algorithms/EulerianPathTest.java
@@ -3,9 +3,11 @@ package algorithms;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -24,8 +26,8 @@ public class EulerianPathTest {
 		assertEquals(10, g.V());
 		assertEquals(14, g.E());
 
-		EulerianPath eulerianPath = new EulerianPath(g);
-		assertTrue(eulerianPath.hasEulerianPath());
+		EulerianCycle eulerianPath = new EulerianCycle(g);
+		assertTrue(eulerianPath.hasEulerianCycle());
 	}
 
 	@Test
@@ -34,8 +36,8 @@ public class EulerianPathTest {
 				TEST_RESOURCES + "EulerPathWeighted_small")));
 		g.addEdge(new Edge(5, 4, 1));
 
-		EulerianPath eulerianPath = new EulerianPath(g);
-		assertFalse(eulerianPath.hasEulerianPath());
+		EulerianCycle eulerianPath = new EulerianCycle(g);
+		assertFalse(eulerianPath.hasEulerianCycle());
 	}
 
 	@Test
@@ -43,8 +45,8 @@ public class EulerianPathTest {
 		EdgeWeightedGraph g = new EdgeWeightedGraph(new In(new File(
 				TEST_RESOURCES + "EulerPath_with_orphans")));
 
-		EulerianPath eulerianPath = new EulerianPath(g);
-		assertTrue(eulerianPath.hasEulerianPath());
+		EulerianCycle eulerianPath = new EulerianCycle(g);
+		assertTrue(eulerianPath.hasEulerianCycle());
 	}
 
 	@Test
@@ -55,8 +57,8 @@ public class EulerianPathTest {
 		// Connect orphan edges to get disconnected graph.
 		g.addEdge(new Edge(10, 11, 1));
 
-		EulerianPath eulerianPath = new EulerianPath(g);
-		assertFalse(eulerianPath.hasEulerianPath());
+		EulerianCycle eulerianPath = new EulerianCycle(g);
+		assertFalse(eulerianPath.hasEulerianCycle());
 	}
 
 	@Test
@@ -66,30 +68,63 @@ public class EulerianPathTest {
 		assertEquals(10, g.V());
 		assertEquals(14, g.E());
 
-		EulerianPath eulerianPath = new EulerianPath(g);
-		assertNotNull(eulerianPath.eulerianPath());
+		EulerianCycle eulerianPath = new EulerianCycle(g);
+		assertNotNull(eulerianPath.eulerianCycle());
 		
-		for(int i : eulerianPath.eulerianPath()) {
-			System.out.print(" : " + i );
-		}
-		
-		System.out.println();
 	}
 
 	@Test
 	public void test_euler_path2() {
 		EdgeWeightedGraph g = new EdgeWeightedGraph(new In(new File(
 				TEST_RESOURCES + "EulerPathWeighted2_small")));
-		assertEquals(10, g.V());
-		assertEquals(14, g.E());
 
-		EulerianPath eulerianPath = new EulerianPath(g);
-		assertNotNull(eulerianPath.eulerianPath());
+		EulerianCycle eulerianPath = new EulerianCycle(g);
+		assertNotNull(eulerianPath.eulerianCycle());
 		
-		for(int i : eulerianPath.eulerianPath()) {
+		for(int i : eulerianPath.eulerianCycle()) {
 			System.out.print(" : " + i );
 		}
 	}
+	
+	@Test
+	public void test_euler_path_3v() {
+		EdgeWeightedGraph g = new EdgeWeightedGraph(new In(new File(
+				TEST_RESOURCES + "EulerPath_3v.txt")));
+		assertEquals(3, g.V());
+		assertEquals(3, g.E());
 
+		EulerianCycle eulerianPath = new EulerianCycle(g);
+		assertTrue(eulerianPath.hasEulerianCycle());
+
+		List<Integer> eulerCycle = eulerianPath.eulerianCycle(); 
+		
+		assertNotNull(eulerCycle);
+		assertEquals(4, eulerCycle.size());
+	}
+
+	@Test
+	public void test_euler_path_5v() {
+		EdgeWeightedGraph g = new EdgeWeightedGraph(new In(new File(
+				TEST_RESOURCES + "EulerPath_5v.txt")));
+
+		EulerianCycle eulerianPath = new EulerianCycle(g);
+		assertTrue(eulerianPath.hasEulerianCycle());
+
+		List<Integer> eulerCycle = eulerianPath.eulerianCycle(); 
+		
+		assertNotNull(eulerCycle);
+		assertEquals(7, eulerCycle.size());
+	}
+
+	@Test
+	public void test_euler_path_4v_neg() {
+		EdgeWeightedGraph g = new EdgeWeightedGraph(new In(new File(
+				TEST_RESOURCES + "EulerPath_4v_neg.txt")));
+
+		EulerianCycle eulerianPath = new EulerianCycle(g);
+		
+		assertFalse(eulerianPath.hasEulerianCycle());
+		assertNull(eulerianPath.eulerianCycle());
+	}
 	
 }
